dad29403ad16c68a57be9e1c7f4a9784eb8a16b9
ryan_ramos <ryanrodriguesramos1@gmail.com>
2023-11-22 22:26:44 -0300

    Versão 1

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from StoreApp.models import Departamento
 
 # Create your views here.
+
+
+def index(request):
+
+    departamentos = Departamento.objects.all()
+    context ={
+        'departamentos' : departamentos
+    }
+    return render(request, 'index.html')
+
+def produto_lista(request):
+    return render(request, 'produtos.html')
+
+def produto_detalhe(request):
+    return render(request, 'produto_detalhes.html')
